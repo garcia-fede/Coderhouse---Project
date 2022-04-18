12b0050915e1f562bd89156a43c16d4b1d80b0ca
Federico Garcia <federicoignaciogarcia@gmail.com>
2022-04-17 21:24:17 -0300

    Minima optimizacion

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -104,36 +104,3 @@ const ItemListContainer = () => {
 }
 
 export default ItemListContainer
-
-
-
-      // const cargaDeDatos = new Promise ((res,rej)=>{
-      //   toast.loading("Los productos se estan cargando")
-      //   setTimeout(()=>{
-      //     toast.dismiss()
-      //     res(productosDeCarga)
-      //   },500)
-      // })
-      // cargaDeDatos
-      // .then((resultado)=>{
-      //   setProductos(resultado)
-      //   if(categoryId=="Calzado"){
-      //     const calzado = resultado.filter(producto=>producto.categoria=="Zapatillas")
-      //     setProductos(calzado)
-      //   }
-      //   else if(categoryId=="Pantalones"){
-      //     const pantalones = resultado.filter(producto=>producto.categoria=="Pantalones")
-      //     setProductos(pantalones)
-      //   }
-      //   else if(categoryId=="Abrigos"){
-      //     const abrigos = resultado.filter(producto=>producto.categoria=="Abrigos")
-      //     setProductos(abrigos)
-      //   }
-      //   else if(categoryId=="Remeras"){
-      //     const remeras = resultado.filter(producto=>producto.categoria=="Remeras")
-      //     setProductos(remeras)
-      //   }
-      // })
-      // .catch((error)=>{
-      //   toast.error("No se pudo cargar los productos correctamente")
-      // })
